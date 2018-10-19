131c79484c4e99331dd7baf6aa85bbf751594f02
Sheila Stephen <sheila.je@gmail.com>
2018-10-19 11:23:24 -0700

    add the branching

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,22 @@ $(document).ready(function(){
   $("#formid").submit(function(event){
     event.preventDefault();
     var name =$("input#name").val();
-    // console.log(name);
     var ansone=$("input:radio[name=q1]:checked").val();
-    anstwo=$("input:radio[name=q2]:checked").val();
-    ansthree=$("input:radio[name=q3]:checked").val();
-    ansfour=$("input:radio[name=q4]:checked").val();
-    ansfive=$("input:radio[name=q5]:checked").val();
+    var anstwo=$("input:radio[name=q2]:checked").val();
+    var ansthree=$("input:radio[name=q3]:checked").val();
+    var ansfour=$("input:radio[name=q4]:checked").val();
+    var ansfive=$("input:radio[name=q5]:checked").val();
 
-    console.log(ansone,anstwo,ansthree,ansfour,ansfive);
+    // console.log(ansone,anstwo,ansthree,ansfour,ansfive);
+
+    if ((ansone==FED)&&(anstwo==yes)&&
+    (ansthree==no)&&(ansfour==no)&&
+    (ansfive==no))
+    {
+        $("#option1").show();
+    }else {
+      $("#option2").show();
+    }
+
   });
 });
